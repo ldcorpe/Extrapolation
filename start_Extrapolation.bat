44c621f49e5504df5c26ae810c4b23739bedc5f8
Gordon Watts <gordon@gordonandpaula.net>
2016-07-11 00:13:35 -0700

    Need to get this out of the central ppro!

--- start_Extrapolation.bat
+++
@@ -1,4 +0,0 @@
-@REM Use to start devenv
-@set ROOTSYS=C:\Users\Gordon\AppData\Local\Temp\root\root-5.34.36-vc12
-@set PATH=C:\Users\Gordon\AppData\Local\Temp\root\root-5.34.36-vc12\bin;%PATH%
-@C:\Users\Gordon\Documents\Code\calratio2015\Extrapolation\Extrapolation.sln
