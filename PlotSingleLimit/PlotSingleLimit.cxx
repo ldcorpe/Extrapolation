62797d07795c568fd065aadba396108780d26a3c
Gordon Watts <gordon@gordonandpaula.net>
2016-07-17 22:21:04 -0700

    First go at adding a single limit plotter framework.

---
+++ PlotSingleLimit/PlotSingleLimit.cxx
@@ -0,0 +1,13 @@
+///
+/// Given input efficiency curves as a function of lifetime, and the result of a limit setting
+/// (the 95% CL, 1 sigma, 2 sigma), generate a final limit plot.
+///
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "hi" << endl;
+	return 1;
+}
