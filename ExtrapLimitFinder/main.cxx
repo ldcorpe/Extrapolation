52431d832373a0c9ef6a06757c0c343c413cdf25
Gordon Watts <gordon@gordonandpaula.net>
2016-08-01 23:19:27 -0400

    By-Hand nuget and also make sure only x86 builds

---
+++ ExtrapLimitFinder/main.cxx
@@ -0,0 +1,14 @@
+//
+// Given an extrapolation file as input, derive the limits as a function of
+// lifetime for output.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv)
+{
+	cout << "hi" << endl;
+	return 0;
+}
