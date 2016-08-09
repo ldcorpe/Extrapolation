7730ce7ced562d677811d7a2d91d2c08e7336ad0
Gordon Watts <gordon@gordonandpaula.net>
2016-08-09 00:38:18 -0500

    Protected how big an item we write into the limit plots.

--- LimitCommonCode/limit_output_file.h
+++ LimitCommonCode/limit_output_file.h
@@ -11,6 +11,10 @@
 #include <vector>
 #include <memory>
 
+inline double protected_val(double val) {
+	return val > 10000 ? 10000 : val;
+}
+
 inline void write_limit_output_file(const abcd_limit_config &lconfig, const std::vector<limit_result> &results)
 {
 	auto f = std::unique_ptr<TFile>(TFile::Open(lconfig.fileName.c_str(), "RECREATE"));
@@ -22,11 +26,11 @@ inline void write_limit_output_file(const abcd_limit_config &lconfig, const std:
 	auto h_n2sigma = new TH1D("sigma_n2", "sigma_n2", results.size(), 0.0, results.size());
 
 	for (int i = 0; i < results.size(); i++) {
-		h_95->SetBinContent(i + 1, results[i].cl_95 * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]);
-		h_p1sigma->SetBinContent(i + 1, results[i].cl_p1sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]);
-		h_p2sigma->SetBinContent(i + 1, results[i].cl_p2sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]);
-		h_n1sigma->SetBinContent(i + 1, results[i].cl_n1sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]);
-		h_n2sigma->SetBinContent(i + 1, results[i].cl_n2sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]);
+		h_95->SetBinContent(i + 1, protected_val (results[i].cl_95 * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]));
+		h_p1sigma->SetBinContent(i + 1, protected_val(results[i].cl_p1sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]));
+		h_p2sigma->SetBinContent(i + 1, protected_val(results[i].cl_p2sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]));
+		h_n1sigma->SetBinContent(i + 1, protected_val(results[i].cl_n1sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]));
+		h_n2sigma->SetBinContent(i + 1, protected_val(results[i].cl_n2sigma * results[i].signal.signalEvents.A / results[i].signal.efficiency[A]));
 	}
 
 	f->Write();
