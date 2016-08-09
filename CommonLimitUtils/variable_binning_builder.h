93424fade75213b8d857ffcee7206ec5ec4b391d
Gordon Watts <gordon@gordonandpaula.net>
2016-08-09 16:48:32 -0500

    Create common code to share more items

---
+++ CommonLimitUtils/variable_binning_builder.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+
+// Helper class to make making variable binning easy
+class variable_binning_builder {
+public:
+	variable_binning_builder(double low)
+	{
+		_v.push_back(low);
+	}
+	void add_bins(int number, double bin_width)
+	{
+		for (int i = 0; i < number; i++) {
+			_v.push_back(*(_v.end() - 1) + bin_width);
+		}
+	}
+	void add_binning_interval(double interval, double bin_width)
+	{
+		for (double count = 0.0; count <= bin_width; count++)
+		{
+			_v.push_back(*(_v.end() - 1) + bin_width);
+		}
+	}
+	void bin_up_to(double max_value, double bin_width)
+	{
+		while (*(_v.end() - 1) < max_value) {
+			_v.push_back(*(_v.end() - 1) + bin_width);
+		}
+	}
+	// Number of bins to delcare to ROOT - note it is one less, as we have to
+	// give it the upper adn lower boundaries of the last and first bin!
+	int nbin() const { return _v.size() - 1; }
+	double *bin_list() const { return (double*)&(_v[0]); }
+
+private:
+	std::vector<double> _v;
+};
