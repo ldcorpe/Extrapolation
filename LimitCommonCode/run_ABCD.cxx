752f0051d067db89b2a9d56bac7551c5f9c1dcc5
Gordon Watts <gordon@gordonandpaula.net>
2016-08-15 11:47:00 +0200

    FIx up the # of parameter points in the scan.

--- LimitCommonCode/run_ABCD.cxx
+++ LimitCommonCode/run_ABCD.cxx
@@ -605,8 +605,8 @@ HypoTestInvTool::LimitResults simultaneousABCD(const Double_t n[4], const Double
 	//              = 6 Number of observed event as test statistic
 
 	Double_t par_poi_min = 0.0;   // mu scanned from par_poi_min to par_poi_max with par_npointscan steps
-	Double_t par_poi_max = 2.0;
-	Int_t    par_npointscan = 100; // default: 100
+	Double_t par_poi_max = 1.0;
+	Int_t    par_npointscan = 500; // default: 100
 
 	auto score = StandardHypoTestInvDemo(0, "", out_filename, "wspace", "mc", "mc", "obsData", calculationType, testStatType, true, par_npointscan, par_poi_min, par_poi_max, par_ntoys);
 
