d8370e62f3dbd7e1700cddb9c162727b29990536
Gordon Watts <gordon@gordonandpaula.net>
2016-08-28 14:16:44 +0200

    Remove the PDF uncertainty from the limit.

--- ExtrapLimitFinder/main.cxx
+++ ExtrapLimitFinder/main.cxx
@@ -197,7 +197,6 @@ config parse_command_line(int argc, char **argv)
 	result.limit_settings.systematic_errors["lumi"] = 0.021; // Final lumi is 2.1%
 
 	// Constant MC errors.
-	result.limit_settings.systematic_errors["mc_PDF"] = 0.02;
 	result.limit_settings.systematic_errors["mc_ISRFSR"] = 0.02;
 
 	// Constant ABCD errors
